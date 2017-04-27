d8078c1ab12394b9e0a3e3282935cbb893f1892a
Clifford Nelson <cliffordryannelson@gmail.com>
2017-04-26 19:18:00 -0700

    add automatic updating copyright in footer - starting from 2012 to current year “©2012 - 2017 Clifford Ryan Nelson. All rights reserved.”

--- components/footer.php
+++ components/footer.php
@@ -2,38 +2,17 @@
 
 	<div class="row footer-top">
 
-<!-- 		<div class="col-xs-12 col-sm-4">
-			<h3 class="footer-headline">Marketing Projects</h3>
-			<p class="footer-item">construction company</p>
-			<p class="footer-item">non profit fundraiser</p>
-			<p class="footer-item">skewd single page</p>
-		</div>
-		<div class="col-xs-12 col-sm-4">
-			<h3 class="footer-headline">Games</h3>
-			<p class="footer-item">game</p>
-			<p class="footer-item">game</p>
-			<p class="footer-item">game</p>
-		</div>
-		<div class="col-xs-12 col-sm-4">
-			<h3 class="footer-headline">Social Things</h3>
-			<p class="footer-item">LinkedIn</p>
-			<p class="footer-item">Facebook</p>
-			<p class="footer-item">InstaGram</p>
-			<p class="footer-item">eMail</p>
-		</div>-->
-	
-
 		<div class="col-xs-12">
 			<p>
-				Thanks for scrolling all the way to the bottom! Check back soon, I'm making updates regularly. 
+				Thanks for scrolling all the way to the bottom! Check back soon, I'm making updates regularly.
 			</p>
 		</div>
 
-	</div> 
+	</div>
 
 	<div class="footer-bottom">
 		<p class="text-right">
-			Clifford Ryan Nelson &copy;2016
+			&copy;<?php auto_copyright("2013"); ?> Clifford Ryan Nelson. All rights reserved.
 		</p>
 	</div>
 
