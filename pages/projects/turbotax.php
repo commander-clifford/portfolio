cd9c0264bacbf55931c75669f42637ab742fba45
Clifford Nelson <cliffordryannelson@gmail.com>
2017-11-20 12:15:28 -0800

    content update: adding project Turbotax

---
+++ pages/projects/turbotax.php
@@ -0,0 +1,22 @@
+<header class="work-header">
+	<h1 class="work-header-headline heading-typ-1 wp">Intuit Turbotax</h1>
+	<p class="work-header-subheadline">Front-End Design/Development for Turbotax.com</p>
+</header>
+
+<div class="row work-content">
+	<div class="col-xs-12 col-md-8">
+		<div class="work-content-display">
+			<a href="https://turbotax.intuit.com/" class="" target="_blank">
+				<img class="img-responsive" src="img/ttcom1.png">
+			</a>
+			<div class="button-row row">
+				<a class="button left" href="https://turbotax.intuit.com/" target="_blank">View live project</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-12 col-md-4">
+		<p>
+			While working with Intuit I was on a team responsible for the Turbo homepage.
+		</p>
+	</div>
+</div>
