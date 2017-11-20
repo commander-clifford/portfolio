7ab27dbe7b95d3e8ca7059f8f6ca8daebafe7535
Clifford Nelson <cliffordryannelson@gmail.com>
2017-11-20 12:28:07 -0800

    adding non breaking spaces in footer content to strategically line break on small screens

--- components/footer.php
+++ components/footer.php
@@ -2,7 +2,7 @@
 
 	<div class="row">
 		<div class="col-xs-12">
-			<h1 class="text-center">Find me on social media</h1>
+			<h1 class="text-center">Find me on social&nbsp;media</h1>
 			<div class="social-wrapper">
 				<a class="social-item" href="https://www.linkedin.com/in/clifford-nelson-bba47459" target="_blank">
 					<span class="icon cliphicon-linkedin"></span>
@@ -30,7 +30,7 @@
 
 	<div class="footer-bottom">
 		<p class="text-right">
-			&copy;<?php auto_copyright("2012"); ?> Clifford Ryan Nelson. All rights reserved.
+			&copy;<?php auto_copyright("2012"); ?> Clifford&nbsp;Ryan&nbsp;Nelson. All&nbsp;rights&nbsp;reserved.
 		</p>
 	</div>
 
