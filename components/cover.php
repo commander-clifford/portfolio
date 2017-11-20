f576a85d12a5eeee5ff5312dfaf9f521b94479d8
Clifford Nelson <cliffordryannelson@gmail.com>
2017-11-20 12:27:19 -0800

    updating cover copy and structure. new copy might be more accurate to my skills set. new structure (wrapped headline and copy in another element) helps support scrolling sudo parallax cover animation

--- components/cover.php
+++ components/cover.php
@@ -1,6 +1,8 @@
 <div id="cover" class="cover">
 	<div id="cover-container" class="cover-container container">
-		<h1 id="cover-headline" class="cover-headline">Clifford</h1>
-		<p id="cover-subheadline" class="cover-subheadline">Experience Designer &amp; Front-End Software Engineer</p>
+		<div id="cover-blur" class="cover-blur">
+			<h1 id="cover-headline" class="cover-headline">Clifford</h1>
+			<p id="cover-subheadline" class="cover-subheadline">Product Designer &amp; Prototyper</p>
+		</div>
 	</div>
 </div>
